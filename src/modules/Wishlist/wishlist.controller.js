186a7fa6b2635c89bdee9bbdbcccbcbc17529089
mohamedd2003 <105282528+mohamedd2003@users.noreply.github.com>
2025-04-03 16:11:08 +0200

    Refactor wishlist controller to toggle product addition/removal and update routes accordingly

--- src/modules/Wishlist/wishlist.controller.js
+++ src/modules/Wishlist/wishlist.controller.js
@@ -7,24 +7,22 @@ export const addProductToWishlist=catchError(async(req,res,next)=>{
     let{product}=req.body
     let prod= await Product.findById(product)
     if(!prod)return next(new AppError("NO Product Exist To add To Wishlist ",404))
-    let {wishlist}=await User.findByIdAndUpdate(req.user._id,{$addToSet:{wishlist:product}},{new:true})
-    console.log(wishlist);
-    
-    if(!wishlist)return next(new AppError("NO ",404))
 
-
-        res.json({message:"success",wishlist})
-})
-export const deleteProductFromWishlist=catchError(async(req,res,next)=>{
-    let{product}=req.body
-    let prod= await Product.findById(product)
-    if(!prod)return next(new AppError("NO Product Exist To add To Wishlist ",404))
+        if(req.user.wishlist.includes(product)){
     let {wishlist}=await User.findByIdAndUpdate(req.user._id,{$pull:{wishlist:product}},{new:true})
-    if(!wishlist)return next(new AppError("NO ",404))
-        res.json({message:"success",wishlist})
+    res.json({message:"success",wishlist})
+
+        }
+
+        else{
+            let {wishlist}=await User.findByIdAndUpdate(req.user._id,{$addToSet:{wishlist:product}},{new:true})
+            res.json({message:"success",wishlist})
+        }
+
+
+
 })
 export const getUserWishlist=catchError(async(req,res,next)=>{
-
 
     let {wishlist}=await User.findById(req.user._id).populate('wishlist')
     if(!wishlist)return next(new AppError("NO ",404))
--- src/modules/Wishlist/wishlist.routes.js
+++ src/modules/Wishlist/wishlist.routes.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { addProductToWishlist, deleteProductFromWishlist, getUserWishlist } from "./wishlist.controller.js";
+import { addProductToWishlist, getUserWishlist } from "./wishlist.controller.js";
 import { auth } from "../../middlewares/Auth/auth.js";
 import { allowedTo } from "../../middlewares/Auth/allowedTo.js";
 
 const wishlistRouter=Router()
 
 wishlistRouter.route('/').patch(auth,allowedTo("user",'admin'),addProductToWishlist)
-.delete(auth,allowedTo("user",'admin'),deleteProductFromWishlist)
 .get(auth,allowedTo("user",'admin'),getUserWishlist)
 export default wishlistRouter
