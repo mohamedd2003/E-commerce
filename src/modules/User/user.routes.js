5fcca89b4d557722597182419d948340b20aa3a1
mohamedd2003 <105282528+mohamedd2003@users.noreply.github.com>
2025-04-22 20:39:09 +0200

    Refactor user routes: reorganize imports for better clarity and consistency

--- src/modules/User/user.routes.js
+++ src/modules/User/user.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import { auth } from "../../middlewares/Auth/auth.js";
+import { addUser, deleteUser, getAlluser, getUserById, updateUser } from "./user.controller.js";
+import { allowedTo } from "../../middlewares/Auth/allowedTo.js";
+import { uploadSingleFile } from "../../fileUpload/fileUplaod.js";
+import { validationSchema } from "../../middlewares/validation/validationSchema.js";
+import { addUserSchema, deleteUserSchema, updateUserSchema } from "./user.validation.js";
 import orderRouter from "../Order/order.routes.js";
-
-const userRouter = Router();
-export default userRouter
-// Sub-router for orders
+import { checkEmailExist } from "../../middlewares/Email/checkEmailExist.js";
+const userRouter=Router()
 userRouter.use('/:id/orders', orderRouter);
 
 // 🟢 Specific routes FIRST
@@ -14,3 +18,4 @@ userRouter.post('/add', auth, allowedTo('admin'), checkEmailExist, uploadSingleF
 userRouter.delete('/:id', auth, allowedTo('admin'), validationSchema(deleteUserSchema), deleteUser);
 userRouter.put('/:id', auth, allowedTo('admin'), validationSchema(updateUserSchema), updateUser);
 userRouter.get('/:id', auth, allowedTo('admin'), getUserById);
+export default userRouter
