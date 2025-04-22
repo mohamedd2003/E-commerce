9305fb6ab917c98414a6f5e2b3f384427762a32d
mohamedd2003 <105282528+mohamedd2003@users.noreply.github.com>
2025-04-22 17:22:47 +0200

    Refactor server configuration: update port assignment to use environment variable; ensure dotenv is configured for environment variable access.

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import { globalError } from './src/middlewares/Error/globalError.js'
 import AppError from './src/utils/appError.js'
 import { bootstrap } from './src/modules/bootstrap.js'
 import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config()
 const app = express()
-const port = 3000 
+const port = process.env.port || 3000 
 
 app.use(cors())
 app.use(express.json())
