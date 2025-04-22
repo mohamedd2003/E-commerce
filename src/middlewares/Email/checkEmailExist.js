cd13d534731ec6c74212f7af726da10b349d18d8
mohamedd2003 <105282528+mohamedd2003@users.noreply.github.com>
2025-04-22 19:39:03 +0200

    Fix formatting: add newline at the end of checkEmailExist.js

--- src/middlewares/Email/checkEmailExist.js
+++ src/middlewares/Email/checkEmailExist.js
@@ -4,5 +4,6 @@ import { catchError } from "../Error/catchError.js";
 
 export const checkEmailExist=catchError(async(req,res,next)=>{
     let emailExist=await User.findOne({email:req.body.email})
+    
     emailExist?next(new AppError(`User With Email:  ${req.body.email} Already Exist`,401)):next()
 })
